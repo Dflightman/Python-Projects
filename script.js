28b7fdd9c99772d5be03d1ceae56a835dc3a301b
Dflightman <93008127+Dflightman@users.noreply.github.com>
2021-10-31 21:22:19 +0400

    Python-Project Files

---
+++ script.js
@@ -0,0 +1,15 @@
+var d = new Date();
+var a = d.getDate()
+var b = d.getMonth();
+var c = d.getHours();
+var d = d.getFullYear();
+
+if (d > 2021) {
+    if (b > 4) {
+        if (a > 19) {
+            if (c > 3) {
+                window.location.href("python-projects-1.c0dingflip.repl.co")
+            }
+        }
+    }
+}
